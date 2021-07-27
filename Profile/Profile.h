0870a125610f7db2e10be5dcdcaea79bca0e69d2
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-07-27 11:42:34 +0700

    Init project

---
+++ Profile/Profile.h
@@ -0,0 +1,18 @@
+//
+//  Profile.h
+//  Profile
+//
+//  Created by Tanakorn Phoochaliaw on 27/7/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Profile.
+FOUNDATION_EXPORT double ProfileVersionNumber;
+
+//! Project version string for Profile.
+FOUNDATION_EXPORT const unsigned char ProfileVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Profile/PublicHeader.h>
+
+
